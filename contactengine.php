62696ae033621bbe92f70594efdf24c54e0f34c9
Alejandro Martinez Turrubiartes <am548726@E224L65.local>
2015-12-03 09:51:03 -0600

    Update 1
    Added styling and images to the website

---
+++ contactengine.php
@@ -0,0 +1,42 @@
+<?php
+
+$EmailFrom = "contact@jandro.com";
+$EmailTo = "amartinez402@gmail.com";
+$Subject = "Trayos Contact Form";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "firstName: ";
+$Body .= $firstName;
+$Body .= "\n";
+$Body .= "lastName: ";
+$Body .= $lastName;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contactthanks.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Trayos | Welcome</title>
+<link rel="stylesheet" href="css/foundation.css" />
+<link rel="stylesheet" href="css/main.css" />
+<script src="js/vendor/modernizr.js"></script>
+<script src="//use.typekit.net/noy0mkg.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+</head>
+
+<body>
+<div id="page-wrap">
+  <h2>Your message has been sent!</h2>
+  <p><a href="index.html">Back to Trayos</a></p>
+</div>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+	</script> 
+<script type="text/javascript">
+	_uacct = "UA-68528-29";
+	urchinTracker();
+	</script>
+</body>
+</html>
